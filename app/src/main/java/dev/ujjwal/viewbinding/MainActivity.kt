9d16d04324e57c9bee1041cc1957fa7b4082950e
ujjwal <ujjwalmaity98@gmail.com>
2020-04-07 22:16:47 +0530

    Excess view using 'view binding'

--- app/src/main/java/dev/ujjwal/viewbinding/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/viewbinding/MainActivity.kt
@@ -2,11 +2,20 @@ package dev.ujjwal.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.ujjwal.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = binding.editText.text
+            binding.editText.setText("")
+        }
     }
 }
